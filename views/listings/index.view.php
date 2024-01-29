75c0dcbcf031e29977d69da38e6a6852b3774a8c
erixnL <96608889+erixnL@users.noreply.github.com>
2024-01-29 22:01:11 +1100

    split ui into partials, create views;add controllers and router

--- views/listings/index.view.php
+++ views/listings/index.view.php
@@ -1,14 +1,11 @@
 <?php loadPartial('head'); ?>
 
 <?php loadPartial('navbar'); ?>
-    <!-- Showcase -->
-<?php loadPartial('showcase-search'); ?>  
 
     <!-- Top Banner -->
 <?php loadPartial('top-banner'); ?>     
 
-    <!-- Job Listings -->
-    <section>
+<section>
       <div class="container mx-auto p-4 mt-4">
         <div class="text-center text-3xl mb-4 font-bold border border-gray-300 p-3">Recent Jobs</div>
         <div class="grid grid-cols-1 md:grid-cols-3 gap-4 mb-6">
@@ -191,13 +188,7 @@
             </div>
           </div>
         </div>
-        <a href="/listings" class="block text-xl text-center">
-          <i class="fa fa-arrow-alt-circle-right"></i>
-          Show All Jobs
-        </a>
       </section>
-
-       <!-- Bottom Banner -->
 <?php loadPartial('bottom-banner'); ?>    
      
 <?php loadPartial('footer'); ?>
